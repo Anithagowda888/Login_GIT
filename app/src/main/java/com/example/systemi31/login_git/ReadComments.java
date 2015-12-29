bb594e3a0d2e91c5a616e2960b4ca08bccb141cf
chethan <chetan@sproutssolutions.com>
2015-12-29 15:36:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/systemi31/login_git/ReadComments.java
@@ -0,0 +1,17 @@
+package com.example.systemi31.login_git;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReadComments extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_comments);
+    }
+}
+
